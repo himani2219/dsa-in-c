1dd90db5f1cf9d0bfb4c7c310b17bd4d946b0349
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-24 10:11:03 +0000

    understanding pointers in c

---
+++ arrays/pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    int b =10;
+    int *p;
+    int *q;
+    p=&b;
+    q=&b;
+
+    printf("%d\n",*p);
+    printf("%d\n",p);
+    printf("%d\n",b);
+    printf("%d\n",&b);
+    return 0;
+}
