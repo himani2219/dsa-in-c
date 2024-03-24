fc549d9f7ab1b399dcb54f62b0d2da51395dd6c2
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-24 14:21:52 +0000

    creation of node, insertion at beginning and end

---
+++ linked list/linkedList.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//creating node data structure
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+//function for creation of node
+struct node* createNode(int data){
+    struct node* newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data=data;
+    newnode->next=NULL;
+    return newnode;
+}
+
+//function for inserting node at Beginning
+void insertAtBeg(struct node** head, int data){
+    struct node* newnode = createNode(data);
+    newnode->next=*head;
+    *head = newnode;
+}
+
+//function for inserting node at end
+void insertAtEnd(struct node** head, int data){
+    struct node* newnode = createNode(data);
+    if(*head==NULL){
+        *head = newnode;
+    }
+    else{
+        struct node* temp = *head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next=newnode;
+    }
+}
+
+//main function
+int main(){
+   struct node* head = NULL;
+   insertAtBeg(&head,10);
+   insertAtBeg(&head,1);
+   insertAtBeg(&head,90);
+   insertAtBeg(&head,16);
+
+    return 0;
+}
+
