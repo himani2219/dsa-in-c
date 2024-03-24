b238f3b50ef0950866ff813f25f7efb9a9a01f10
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-24 18:06:43 +0000

    deletion from beg, end and specific position.

--- linked list/linkedList.c
+++ linked list/linkedList.c
@@ -72,6 +72,67 @@ void insertAt(struct node* head){
     }
 }
 
+//function for deletion from beginning
+void delFromBeg(struct node** head){
+    struct node* temp = head;
+    if(*head == NULL){
+        printf("List is Empty!");
+        return;
+    }
+    head=temp->next;
+    free(temp);
+}
+
+//function for deletion from end
+void delFromEnd(struct node** head){
+    struct node* temp = head;
+    if(*head == NULL){
+        printf("List is Empty!");
+    }
+    else if((*head)->next==NULL){
+        free(*head);
+        *head=NULL;
+    }
+    else{
+        while(temp->next->next!=NULL){
+            temp=temp->next;
+        }
+        free(temp->next);
+        temp->next=NULL;
+    }
+}
+
+//function for deletion from specific position
+void delFromPos(struct node** head){
+    int ind, count;
+    struct node* temp = *head;
+    if(*head == NULL){
+        printf("List is Empty!");
+        return;
+    }
+    printf("Enter index of element: ");
+    scanf("%d", &ind);
+    if(ind==0){
+        *head=temp->next;
+        free(temp);
+    }
+    else{
+        while (temp!=NULL && count<ind-1)
+        {
+            temp=temp->next;
+            count++;
+        }
+        if(temp==NULL || temp->next==NULL){
+            printf("Invalid position!");
+        }
+        else{
+            struct node* nodeToDelete = temp->next;
+            temp->next=nodeToDelete->next;
+            free(nodeToDelete);
+        }
+    }
+}
+
 //function to print linked list
 void print(struct node* head){
     while(head!=NULL){
@@ -88,7 +149,7 @@ int main(){
 
     do{
         printf("\nSelect the operation to be performed on linked list: ");
-        printf("\n1.\tDisplay list \n2.\tInsertion at beginning. \n3.\tInsertion at end. \n4.\tInsertion at specific position. \n5.\tExit\n");
+        printf("\n1.\tDisplay list \n2.\tInsertion at beginning. \n3.\tInsertion at end. \n4.\tInsertion at specific position. \n5.\tDelete from beginning. \n6.\tDelete from end. \n7.\tDelete from specific index. \n8.\tExit\n");
         scanf("%d", &option);
         switch (option)
         {
@@ -100,8 +161,14 @@ int main(){
             break;
         case 4 : insertAt(head);
             break;
-        case 5 : return 0;
-                 break;
+        case 5 : delFromBeg(head);
+            break;
+        case 6 : delFromEnd(head);
+            break;
+        case 7 : delFromPos(head);
+            break;
+        case 8 : return 0;
+            break;
         
         default: printf("Invalid option: ");
             break;
