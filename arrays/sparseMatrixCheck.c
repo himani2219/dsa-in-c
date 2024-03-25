7271c03162d01d460f69a4d48e8245c33b2272b2
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-25 18:19:07 +0000

    .

--- arrays/sparseMatrixCheck.c
+++ arrays/sparseMatrixCheck.c
@@ -5,24 +5,24 @@ int main(){
     int cols;
     int count=0;
     printf("\nEnter rows of matrix: ");
-        scanf("%d", &rows);
-        printf("Enter columns of matrix: ");
-        scanf("%d", &cols);
-        int arr[rows][cols];
+    scanf("%d", &rows);
+    printf("Enter columns of matrix: ");
+    scanf("%d", &cols);
+    int arr[rows][cols];
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                printf("Enter element at [%d][%d]: ",i+1,j+1);
-                scanf("%d", &arr[i][j]);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            printf("Enter element at [%d][%d]: ",i+1,j+1);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(arr[i][j]==0){
+                count++;
             }
         }
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(arr[i][j]==0){
-                    count++;
-                }
-            }
-        }
+    }
     if(count>(rows*cols)/2){
         printf("It is a sparse matrix.\n");
     }
