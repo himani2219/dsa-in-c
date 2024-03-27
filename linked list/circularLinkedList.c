1b82a7f6f1346d4ac1753ba4808bc733e50b7be7
Himani <102609895+himani2219@users.noreply.github.com>
2024-03-27 18:08:48 +0000

    .

--- linked list/circularLinkedList.c
+++ linked list/circularLinkedList.c
@@ -34,11 +34,12 @@ struct LinkedList* createLinkedList(){
 void insertAtBeg(struct LinkedList* list){
     int data;
     printf("Enter element to be inserted: ");
-    scanf("%d", data);
+    scanf("%d", &data);
     struct node* newnode = createNode(data);
     if(list->head==NULL){
         list->head=newnode;
         list->tail=newnode;
+        newnode->next=NULL;
     }
     else{
         newnode->next=list->head;
@@ -51,7 +52,7 @@ void insertAtBeg(struct LinkedList* list){
 void insertAtEnd(struct LinkedList* list){
     int data;
     printf("Enter element to be inserted: ");
-    scanf("%d", data);
+    scanf("%d", &data);
     struct node* newnode = createNode(data);
     if(list->head==NULL){
         list->head=newnode;
@@ -68,7 +69,7 @@ void insertAtEnd(struct LinkedList* list){
 void insertAt(struct LinkedList* list){
     int data, ind;
     printf("Enter index: ");
-    scanf("%d", ind);
+    scanf("%d", &ind);
 
     if(ind<0){
         printf("Invalid index!");
@@ -76,7 +77,7 @@ void insertAt(struct LinkedList* list){
     }
 
     printf("Enter element to be inserted: ");
-    scanf("%d", data);
+    scanf("%d", &data);
     struct node* newnode = createNode(data);
 
     if(ind==0){        
@@ -114,4 +115,55 @@ void insertAt(struct LinkedList* list){
         newnode->next=temp->next;
         temp->next=newnode;
     }
+}
+
+//traverse list
+void print(struct LinkedList* list){
+    if (list->head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct node* temp = list->head;
+    printf("%d", temp->data);
+    temp=temp->next;
+    while(temp!=list->head){
+        printf("%d ", temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int option;
+    struct LinkedList* list = createLinkedList();  
+    do{
+        printf("\nSelect the operation to be performed on linked list: ");
+        printf("\n1.\tDisplay list \n2.\tInsertion at beginning. \n3.\tInsertion at end. \n4.\tInsertion at specific position. \n5.\tDelete from beginning. \n6.\tDelete from end. \n7.\tDelete from specific index. \n8.\tExit\n");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1 : print(list);
+            break;
+        case 2 : insertAtBeg(list);
+            break;
+        case 3 : insertAtEnd(list);
+            break;
+        case 4 : insertAt(list);
+            break;
+        // case 5 : delFromBeg(list);
+        //     break;
+        // case 6 : delFromEnd(list);
+        //     break;
+        // case 7 : delFromPos(list);
+        //     break;
+        case 8 : return 0;
+            break;
+        
+        default: printf("Invalid option: ");
+            break;
+        }
+    }
+    while(1);
+    return 0;
 }
